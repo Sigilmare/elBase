0d261a4c55ff373d723ef84063412b99e42313e3
Sigilmare <sigilmare@gmail.com>
2023-11-17 21:15:37 +0000

    Clarify valid contributors

--- lua/autorun/elbase_shared.lua
+++ lua/autorun/elbase_shared.lua
@@ -2,6 +2,7 @@ elBase = elBase or {}
 
 elBase.Version = 1.1
 
+-- No. Just because you changed/added small things doesn't mean you should go here.
 elBase.Credits = {
     {"76561198171603670", "elBase Creator"}
 }
