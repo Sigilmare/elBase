674919ff2adb320fb5202f7a59c992993c5941d7
Sigilmare <sigilmare@gmail.com>
2023-11-18 13:42:01 +0000

    a

--- lua/autorun/elbase_shared.lua
+++ lua/autorun/elbase_shared.lua
@@ -5,5 +5,6 @@ elBase.Version = 1.1
 -- No. Just because you changed/added small things doesn't mean you should go here.
 elBase.Credits = {
     {"76561198171603670", "Creator of elBase :D"},
-    {"76561198913877245", "Various GitHub contributions"}
+    {"76561198913877245", "Various GitHub contributions"},
+    
 }
