89a59eb9b6d503e2641a7c6e70d5ce7e5893080c
Sigilmare <94254206+Sigilmare@users.noreply.github.com>
2023-11-20 09:00:53 +0000

    Remove PowerOverNight Gaming blacklist

--- lua/autorun/elbase_shared.lua
+++ lua/autorun/elbase_shared.lua
@@ -10,14 +10,8 @@ elBase.Credits = {
 }
 
 -- Blacklisted users cannot use any of my addons.
-local REASON_AWPONG = "Affiliated with \"PowerOverNight Gaming\""
 local REASON_AWZLC = "Affiliated with \"ZombieLand Community\""
 elBase.Blacklist = {
-    {"76561198205577016", REASON_AWPONG},
-    {"76561198050109523", REASON_AWPONG},
-    {"76561198866986264", REASON_AWPONG},
-    {"76561198274314803", REASON_AWPONG},
-    {"76561198124546592", REASON_AWPONG},
     {"76561198206304921", REASON_AWZLC},
     {"76561198965259619", REASON_AWZLC},
     {"76561199136923537", REASON_AWZLC..", it has to be done."},
@@ -32,6 +26,7 @@ elBase.Blacklist = {
     {"76561198808612658", REASON_AWZLC},
     {"76561198097536249", "No."},
     {"76561199163486631", "No."},
+    {"76561198362239688", "No."}
 }
 elBase.BlacklistIDs = {}
 for index, tab in ipairs(elBase.Blacklist) do
